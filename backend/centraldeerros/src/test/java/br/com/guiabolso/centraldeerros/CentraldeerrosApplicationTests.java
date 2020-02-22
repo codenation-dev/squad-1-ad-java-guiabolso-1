ca6b63a5bff3e6ac734eb06307014f6b759ca1cd
Estela <eombastos@gmail.com>
2020-02-22 12:02:59 -0300

    spring start project

---
+++ backend/centraldeerros/src/test/java/br/com/guiabolso/centraldeerros/CentraldeerrosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.guiabolso.centraldeerros;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CentraldeerrosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
