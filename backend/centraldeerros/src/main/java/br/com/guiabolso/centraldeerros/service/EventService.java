8165ea08137dbddd082a867be616c726fc0e0717
amandabacelli <amanda.sted@gmail.com>
2020-03-01 17:22:25 -0300

    add metodos service

--- backend/centraldeerros/src/main/java/br/com/guiabolso/centraldeerros/service/EventService.java
+++ backend/centraldeerros/src/main/java/br/com/guiabolso/centraldeerros/service/EventService.java
@@ -1,4 +1,29 @@
 package br.com.guiabolso.centraldeerros.service;
 
+import br.com.guiabolso.centraldeerros.entity.Event;
+import br.com.guiabolso.centraldeerros.repositories.EventRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
 public class EventService {
+
+    @Autowired
+    private EventRepository eventRepository;
+
+    public List <Event> findAllOrderByParams(String params) {
+        switch (params) {
+            case "level":
+                return eventRepository.findAllOrderByLevel();
+            case "eventsQuantity":
+                return eventRepository.findAllOrderByEventsQuantity();
+        }
+        return new ArrayList<>();
+    }
+
+
 }
