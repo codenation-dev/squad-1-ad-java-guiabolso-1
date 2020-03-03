68cc13bf0c378ac14473cdd471a704f32f3ef81e
Eveline <51457178+evelinenogueira@users.noreply.github.com>
2020-03-02 23:02:13 -0300

    Add files via upload

--- backend/centraldeerros/src/main/java/br/com/guiabolso/centraldeerros/service/AccountService.java
+++ backend/centraldeerros/src/main/java/br/com/guiabolso/centraldeerros/service/AccountService.java
@@ -1,4 +1,23 @@
 package br.com.guiabolso.centraldeerros.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import br.com.guiabolso.centraldeerros.repositories.EventRepository;
+
+@Service
 public class AccountService {
+
+    @Autowired
+    private AccountRepository accountRepository;
+
+    public Account add(Account object) {
+
+        return accountRepository.save(Account) object);
+
+    public Account get(Account id) {
+
+        retunr accountRepository.findById(id);
+        }
+
+
+    }
 }
--- backend/centraldeerros/src/main/java/br/com/guiabolso/centraldeerros/service/EventService.java
+++ backend/centraldeerros/src/main/java/br/com/guiabolso/centraldeerros/service/EventService.java
@@ -1,4 +1,22 @@
 package br.com.guiabolso.centraldeerros.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import br.com.guiabolso.centraldeerros.repositories.EventRepository;
+
+@Service
 public class EventService {
+
+    @Autowired
+    private EventService eventRepository;
+
+    public Event add(Event object) {
+
+        return eventRepository.save(Event) object);
+
+    public Event get(Event id) {
+
+        return eventRepository.findByID(id);
+        }
+
+    }
 }
