dece009edf3307e34157a7bd835ff6ba4a583fb5
Estela Bastos <eombastos@gmail.com>
2020-03-06 22:53:33 -0300

    list events

--- backend/centraldeerros/src/main/java/br/com/guiabolso/centraldeerros/repositories/EventRepository.java
+++ backend/centraldeerros/src/main/java/br/com/guiabolso/centraldeerros/repositories/EventRepository.java
@@ -1,9 +1,12 @@
 package br.com.guiabolso.centraldeerros.repositories;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import br.com.guiabolso.centraldeerros.entity.Event;
 
 public interface EventRepository extends JpaRepository<Event, Long> {
-
+	Page<Event> findAll(Specification<Event> spec, Pageable pageable);
 }
