29bc67a29898e6d5be7cf7df0c9b0462b2f239c9
Amanda S. M. Bacelli <amanda.sted@gmail.com>
2020-03-21 15:19:22 -0300

    Revert "Registros para teste"

--- squad-1-ad-java-guiabolso-1/backend/centraldeerros/src/main/java/br/com/guiabolso/centraldeerros/repositories/EventRepository.java
+++
@@ -1,9 +0,0 @@
-package br.com.guiabolso.centraldeerros.repositories;
-
-import org.springframework.data.repository.CrudRepository;
-
-import br.com.guiabolso.centraldeerros.entity.Event;
-
-public interface EventRepository extends CrudRepository<Event, Long> {
-
-}
