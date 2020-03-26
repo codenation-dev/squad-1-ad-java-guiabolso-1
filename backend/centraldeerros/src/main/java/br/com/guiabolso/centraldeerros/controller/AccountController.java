12c80521551d18d6cefa95d2d45f840c6498ce6e
nadiardgs <nadia.r1@outlook.com>
2020-03-25 23:23:05 -0300

    Adicionado pacote validation com os eventos para Login do módulo Account

--- backend/centraldeerros/src/main/java/br/com/guiabolso/centraldeerros/controller/AccountController.java
+++ backend/centraldeerros/src/main/java/br/com/guiabolso/centraldeerros/controller/AccountController.java
@@ -3,21 +3,29 @@ package br.com.guiabolso.centraldeerros.controller;
 import br.com.guiabolso.centraldeerros.dto.AccountDTO;
 import br.com.guiabolso.centraldeerros.entity.Account;
 import br.com.guiabolso.centraldeerros.service.AccountService;
+import br.com.guiabolso.centraldeerros.validation.AccountValidator;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 
 import javax.validation.Valid;
 
 @Controller
 @RequestMapping("/centralerros/account")
 public class AccountController {
-
+	
     @Autowired
     AccountService accountService;
+    
     AccountDTO accountDTO;
+    
+    @Autowired
+    private AccountValidator accountValidator;
 
     @PostMapping
     public ResponseEntity<Account> saveAccount(@Valid @RequestBody Account account){
@@ -28,7 +36,38 @@ public class AccountController {
             return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
         }
     }
+    
+    @GetMapping("/registration")
+    public String register(Model model)
+    {
+    	model.addAttribute("accountForm", new Account());
+    	return "registration";
+    }
+    
+    @PostMapping("/registration")
+    public String register(@ModelAttribute("accountForm") Account accountForm, BindingResult bindingResult)
+    {
+    	accountValidator.validate(accountForm, bindingResult);
+    	if (bindingResult.hasErrors())
+    		return "registration";
+    	
+    	accountService.save(accountForm);
+    	accountService.doLogin(accountForm.getUsername(), accountForm.getPassword());
+    	
+    	return "redirect:/welcome";
+    }
 
+    @GetMapping("/login")
+    public String login(Model model, String error, String logout) {
+        if (error != null)
+            model.addAttribute("error", "Usuario e senha invalidos.");
+
+        if (logout != null)
+            model.addAttribute("message", "Voce foi deslogado com sucesso.");
+
+        return "login";
+    }
+    
     @GetMapping("/{id}")
     public ResponseEntity<AccountDTO> getById(@PathVariable Long id) {
         try {
---
+++ backend/centraldeerros/src/main/java/br/com/guiabolso/centraldeerros/validation/AccountValidator.java
@@ -0,0 +1,41 @@
+package br.com.guiabolso.centraldeerros.validation;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.Errors;
+import org.springframework.validation.Validator;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.Errors;
+import org.springframework.validation.ValidationUtils;
+
+import br.com.guiabolso.centraldeerros.entity.Account;
+import br.com.guiabolso.centraldeerros.service.AccountService;
+
+public class AccountValidator implements Validator {
+
+	@Autowired
+	private AccountService accountService;
+	
+	@Override
+	public boolean supports(Class<?> aClass) {
+		return Account.class.equals(aClass);
+	}
+
+	@Override
+	public void validate(Object target, Errors errors) {
+		Account account = (Account) target;
+		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "username", "NotEmpty");
+		
+		if (account.getUsername().length() < 6 || account.getUsername().length() > 20)
+			errors.rejectValue("username", "Size.accountForm.username");
+		
+		if (accountService.findByUsername(account.getUsername()) != null)
+			errors.rejectValue("username", "Duplicate.accountForm.username");
+		
+		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "password", "NotEmpty");
+        if (account.getPassword().length() < 8 || account.getPassword().length() > 20) {
+            errors.rejectValue("password", "Size.userForm.password");
+        }
+
+	}
+	
+}
