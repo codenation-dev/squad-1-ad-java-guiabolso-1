8700e83d749cc3a9b1242cc57ac2ea780bbe2cc3
amandabacelli <amanda.bacelli@mercadolivre.com>
2020-04-12 10:45:03 -0300

    correcao login

--- src/main/java/br/com/guiabolso/centraldeerros/controller/LoginController.java
+++ src/main/java/br/com/guiabolso/centraldeerros/controller/LoginController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
 
 @Controller
-@RequestMapping("/api/login")
+@RequestMapping("/api")
 public class LoginController {
 	
     @Autowired
