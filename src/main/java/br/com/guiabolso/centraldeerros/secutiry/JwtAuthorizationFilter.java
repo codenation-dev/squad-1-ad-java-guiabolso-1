1ce6d832e26cc8d01e1eb3d9fceeff60c36ed102
evelinenogueira <evelinecontato@gmail.com>
2020-04-17 18:36:24 -0300

    att

--- src/main/java/br/com/guiabolso/centraldeerros/secutiry/JwtAuthorizationFilter.java
+++ src/main/java/br/com/guiabolso/centraldeerros/secutiry/JwtAuthorizationFilter.java
@@ -1,37 +1,21 @@
 package br.com.guiabolso.centraldeerros.secutiry;
-<<<<<<< HEAD
 import java.io.IOException;
 import java.util.Collections;
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import io.swagger.annotations.ApiKeyAuthDefinition;
-import io.swagger.annotations.SecurityDefinition;
-import io.swagger.annotations.SwaggerDefinition;
-=======
 
 import io.jsonwebtoken.ExpiredJwtException;
 import io.jsonwebtoken.MalformedJwtException;
 import io.jsonwebtoken.UnsupportedJwtException;
->>>>>>> 7be148b7c6c4e245cd93f6c6550367038742a641
+
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
-<<<<<<< HEAD
-import io.jsonwebtoken.ExpiredJwtException;
-import io.jsonwebtoken.MalformedJwtException;
-import io.jsonwebtoken.UnsupportedJwtException;
-=======
 
-import javax.servlet.FilterChain;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.util.Collections;
 
 @Component
 
